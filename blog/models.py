5e515547772e8e462dffea0a9be9916b40b00e5d
Aida <mehrniaaida@gmail.com>
2024-03-17 16:47:22 +0330

    1.5 Done

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,19 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    class StatusChoices(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     body = models.TextField()
     published_at = models.DateTimeField(default=timezone.now())
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=StatusChoices.choices,
+                              default=StatusChoices.DRAFT)
 
     class Meta:
         ordering = ('-published_at', )
