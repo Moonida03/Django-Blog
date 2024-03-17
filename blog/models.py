5ee749666cdc945d269cc96f8388cb5323a847f1
Aida <mehrniaaida@gmail.com>
2024-03-17 17:07:38 +0330

    1.6 Done

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -16,6 +17,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=StatusChoices.choices,
                               default=StatusChoices.DRAFT)
+    auther = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='posts')
 
     class Meta:
         ordering = ('-published_at', )
