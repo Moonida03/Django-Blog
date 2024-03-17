0f373e7f4c18039a6a68a799bbd31e7c90615aef
Aida <mehrniaaida@gmail.com>
2024-03-17 20:25:27 +0330

    1.8 , 1.9 , 1.10 Done

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_query(self):
+        return super().get_queryset().filter(status='PB')
+
+
 class Post(models.Model):
     class StatusChoices(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -20,6 +25,8 @@ class Post(models.Model):
     auther = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='posts')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-published_at', )
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    queryset = Post.objects.all()
+    return render(request, 'list.html', {'queryset': queryset})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'detail.html', {'post': post})
