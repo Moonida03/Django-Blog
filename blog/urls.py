683cdb3b7048394cc0ad676a5f08e7b29fcfb220
Aida <mehrniaaida@gmail.com>
2024-03-23 16:44:39 +0330

    1.13 Done

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post/', views.post_list),
-    path('post/<int:id>/', views.post_detail)
+    path('post/', views.post_list, name='post_list'),
+    path('post/<int:id>/', views.post_detail, name='post_detail')
 ]
